509c08c2692b080a1d7cbac9e789db80226140eb
Tom Close <tom.g.close@gmail.com>
2022-09-28 09:30:11 +1000

    added option to compress generated nifti files

--- medimages4tests/nifti.py
+++ medimages4tests/nifti.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import gzip
+import shutil
 import numpy as np
 import nibabel as nb
 
@@ -8,10 +10,14 @@ def sample_image(
     data: np.ndarray = None,
     vox_sizes=(1.0, 1.0, 1.0),
     qform=(1, 2, 3, 1),
+    compressed=False,
 ):
     """Create a random Nifti file to satisfy BIDS parsers"""
     if data is None:
         data = np.random.randint(0, 1, size=[10, 10, 10])
+
+    uncompressed = out_file.with_suffix('.nii')
+
     hdr = nb.Nifti1Header()
     hdr.set_data_shape(data.shape)
     hdr.set_zooms(vox_sizes)  # set voxel size
@@ -23,5 +29,15 @@ def sample_image(
             hdr.get_best_affine(),
             header=hdr,
         ),
-        out_file,
+        uncompressed,
     )
+
+    if compressed:
+        out_file = out_file.with_suffix('.nii.gz')
+        with open(uncompressed, 'rb') as f_in:
+            with gzip.open(out_file, 'wb') as f_out:
+                shutil.copyfileobj(f_in, f_out)
+    else:
+        out_file = uncompressed
+
+    return out_file
